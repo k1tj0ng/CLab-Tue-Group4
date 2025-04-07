6ba46906fab000730a89acaa702875237194afba
Ribka Anastasya Sitorus <rsit0332@uni.sydney.edu.au>
2025-04-07 13:04:23 +1000

    timer part a

--- CLab/Src/timer.c
+++ CLab/Src/timer.c
@@ -2,83 +2,43 @@
 #include "timer.h"
 #include <stddef.h>
 
-// Define the variables
-void (*periodic_callback_func)(void) = NULL;
-void (*one_shot_callback_func)(void) = NULL;
-volatile uint32_t one_shot_delay = 0;
-volatile uint32_t one_shot_counter = 0;
-volatile uint32_t current_period = 0;
 
-// Enable the clocks for GPIOE and TIM2
+//Define the callback function
+static CallbackFunction timerCallback = NULL;
+
+// Enable the clocks for GPIO and TIM2
 void enable_clocks() {
-    RCC->AHBENR |= RCC_AHBENR_GPIOEEN;  // Enable the GPIOE clock
-    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // Enable the TIM2 clock
+    RCC->AHBENR |= RCC_AHBENR_GPIOEEN;
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 }
 
-// Initialize the timer with period (ms) and a callback function
-void timer_init(uint32_t period_ms, void (*callback_function)(void)) {
-    periodic_callback_func = callback_function;
-    current_period = period_ms;
-
-    // Configure prescaler for 1 ms time base
-    TIM2->PSC = 7999;			// (8 MHz / 1000 ms) - 1
-    TIM2->ARR = period_ms - 1;	// Set period (ARR = period - 1)
-    TIM2->CNT = 0;				// Reset the counter
-    TIM2->CR1 |= TIM_CR1_CEN;	// Start the timer
-    NVIC_EnableIRQ(TIM2_IRQn);  // Enable the timer interrupt
+// Initialise the discovery board I/O
+void initialise_board() {
+    uint16_t *led_output_registers = ((uint16_t *)&(GPIOE->MODER)) + 1;
+    *led_output_registers = 0x5555;  // Set PE8-PE15 as output
 }
 
-// Reset the timer with a new period
-void timer_reset(uint32_t period_ms) {
-	current_period = period_ms;		// Update the period
-    TIM2->ARR = period_ms - 1;	// Reset the period
-    TIM2->CNT = 0;				// Reset the counter
-}
-
-// Start the timer
-void timer_start(void) {
-    TIM2->CR1 |= TIM_CR1_CEN;
-}
-
-// Stop the timer
-void timer_stop(void) {
-    TIM2->CR1 &= ~TIM_CR1_CEN;
-}
-
-// One-shot timer event with a delay
-void timer_one_shot(uint32_t delay_ms, void (*callback_function)(void)) {
-    one_shot_delay = delay_ms;
-    one_shot_counter = 0;
-    one_shot_callback_func = callback_function;
-
-    TIM2->ARR = 1;				// Set the timer period to 1 tick
-    TIM2->CNT = 0;				// Reset the counter
-    TIM2->CR1 |= TIM_CR1_CEN;	// Start the timer
-    NVIC_EnableIRQ(TIM2_IRQn);	// Enable the timer interrupt
-}
-
-// Timer interrupt handler
+// Timer interrupt handler for TIM2
 void TIM2_IRQHandler(void) {
-    if (TIM2->SR & TIM_SR_UIF) {	// Check if update interrupt flag is set
-        TIM2->SR &= ~TIM_SR_UIF;	// Clear the interrupt flag
-
-        // Debugging: Toggle LED to check if interrupt is triggered
-        GPIOE->ODR ^= 0x1;  // Toggle an LED (PE8 for example) to see if interrupt is firing
-
-        // Handle periodic callback if the timer overflow
-        if (periodic_callback_func) {
-            GPIOE->ODR ^= 0x1;
-            periodic_callback_func();
-        }
-
-        // Handle one-shot callback after the specified delay
-        if (one_shot_callback_func) {
-            one_shot_counter++;
-            if (one_shot_counter >= one_shot_delay) {
-                one_shot_callback_func();
-                TIM2->CR1 &= ~TIM_CR1_CEN;  // Stop the timer after the one-shot event
-            }
-        }
+    if (TIM2->SR & TIM_SR_UIF) {  // Check if the interrupt flag is raised
+        TIM2->SR &= ~TIM_SR_UIF;  // Clear the interrupt flag
+        timerCallback();  // Execute the callback function
     }
 }
 
+// Timer that trigger the callback (blink LED) every x millisecond
+void timer_init(uint32_t interval, CallbackFunction callback) {
+    // Set up the timer prescaler
+    TIM2->PSC = 7999;  // (8MHz:1000)-1
+    TIM2->ARR = interval;
+
+	// Enable interrupt when there is overflow
+	TIM2->DIER |= TIM_DIER_UIE;
+    NVIC_EnableIRQ(TIM2_IRQn);
+
+    // Set the callback function
+    timerCallback = callback;
+
+    // Start the timer
+    TIM2->CR1 |= TIM_CR1_CEN;
+}
