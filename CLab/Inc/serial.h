c29c5789ba344478ee8627472d0586304336a4a2
Kit <166393923+k1tj0ng@users.noreply.github.com>
2025-04-07 11:44:47 +1000

    idk - kit

--- CLab/Inc/serial.h
+++ CLab/Inc/serial.h
@@ -40,6 +40,7 @@ void SerialOutputChar(uint8_t, SerialPort *serial_port);
 // Input: pointer to a NULL-TERMINATED string (if not null terminated, there will be problems)
 void SerialOutputString(uint8_t *pt, SerialPort *serial_port);
  
+ 
 // Callback function types
 //typedef void (*SerialRxCallback)(char* str, uint32_t len);  // RX complete callback
 //typedef void (*SerialTxCallback)(void);                     // TX complete callback
