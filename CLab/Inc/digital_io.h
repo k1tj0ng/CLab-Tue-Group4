b9d773091a2a69efcd1924b4634702a53cbc9cdf
sulfatislam <160558192+sulfatislam@users.noreply.github.com>
2025-04-07 12:38:50 +1000

    Create digital_io.h
    header file for digital_io.c

---
+++ CLab/Inc/digital_io.h
@@ -0,0 +1,18 @@
+// digital_io.h
+#ifndef DIGITAL_IO_H
+#define DIGITAL_IO_H
+
+#include "stdint.h"
+
+//function declarations
+void chase_led();
+void button_press(void(*callback)(void));
+void EXTI0_IRQHandler(void);
+
+// Sets the state of the LEDs
+void set_led_state(uint16_t state);
+
+// Gets the current state of the LEDs
+uint16_t get_led_state(void);
+
+#endif // DIGITAL_IO_H
