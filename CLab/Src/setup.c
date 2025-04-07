c80d7bb8577d514ee94030e8f6e9cd6ec91b89f7
sulfatislam <160558192+sulfatislam@users.noreply.github.com>
2025-04-07 13:07:08 +1000

    Update setup.c

--- CLab/Src/setup.c
+++ CLab/Src/setup.c
@@ -7,3 +7,9 @@ void enable_clocks() {
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN; // Enable the TIM2 clock
 }
 
+// Initialise the discovery board I/O
+void initialise_board() {
+    uint16_t *led_output_registers = ((uint16_t *)&(GPIOE->MODER)) + 1;
+    *led_output_registers = 0x5555;  // Set PE8-PE15 as output
+}
+
