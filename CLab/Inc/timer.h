5c30825220b01ed58c306257c9cb6e24e97222f6
Ribka Anastasya Sitorus <124226902+RibkaAnastasyaSitorus@users.noreply.github.com>
2025-04-08 16:54:09 +1000

    Update timer.h : latest

--- CLab/Inc/timer.h
+++ CLab/Inc/timer.h
@@ -6,13 +6,16 @@
 // Define the callback function using the function pointers
 typedef void (*CallbackFunction)(void);
 
-// Timer software module that can trigger the callback function after certain interval
+// Timer module that can trigger the callback function after a specific interval
 void timer_init(uint32_t interval, CallbackFunction callback);
 
 // Timer interrupt handler
 void TIM2_IRQHandler(void);
 
-// Reset the timer with the new interval
+// Reset the timer with the current interval
 void timer_reset(uint32_t interval);
+
+// Timer module that can trigger one-shot event
+void timer_one_shot(uint32_t delay, CallbackFunction callback);
 
 #endif
