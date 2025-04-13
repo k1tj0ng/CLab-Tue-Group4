245536d181b97588e792ed6548898b68145eb261
Ribka Anastasya Sitorus <rsit0332@uni.sydney.edu.au>
2025-04-14 02:53:18 +1000

    update timer.c final

--- CLab/Src/timer.c
+++ CLab/Src/timer.c
@@ -9,77 +9,80 @@
 //Define the callback function
 CallbackFunction timerCallback = NULL;
 
-// Timer that triggers the callback (blink LED) every x millisecond
-void timer_init(uint32_t interval, CallbackFunction callback) {
-	// Set up the timer prescaler
-	TIM2->PSC = 7999;  // (8MHz:1000)-1
-	TIM2->ARR = interval;
+// Flag to check the mode of the timer (oneshot or periodic)
+volatile bool isOneShot = false;
 
-	// Enable interrupt when there is overflow
+// Timer that triggers the callback (blink_led) every (interval) millisecond
+void timer_init(uint32_t interval, CallbackFunction callback) {
+	TIM2->CR1 &= ~TIM_CR1_CEN;	// Stop the timer
+	isOneShot = false;			// Flag for periodic timer mode
+
+	// Set up the timer prescaler, for 1khZ frequency = (8MHz:1000)-1
+	TIM2->PSC = 7999;
+	TIM2->ARR = interval;		// Set the time interval
+	TIM2->CNT = 0;				// Reset the counter
+
+	TIM2->DIER |= TIM_DIER_UIE;	// Enable interrupt when there is overflow
+	NVIC_EnableIRQ(TIM2_IRQn);	// Enable interrupt in the NVIC
+	
+	timerCallback = callback;	// Set the callback function
+	
+	TIM2->CR1 |= TIM_CR1_CEN;	// Start the timer
+}
+
+// Timer module to reset the timer with a new interval
+void timer_reset(uint32_t new_interval) {
+	TIM2->CR1 &= ~TIM_CR1_CEN;	// Stop timer
+	TIM2->CNT = 0;				// Reset counter
+	TIM2->ARR = new_interval;	// Update ARR
+	TIM2->EGR |= TIM_EGR_UG;	// Force update event to load ARR
+	TIM2->CR1 |= TIM_CR1_CEN;	// Restart timer
+}
+
+// Timer that triggers a one-shot event after (delay) millisecond
+void timer_one_shot(uint32_t delay, CallbackFunction callback) {
+	TIM2->CR1 &= ~TIM_CR1_CEN;	// Stop timer
+	isOneShot = true;			// Flag for oneshot timer mode
+
+	// Set up the timer prescaler
+	TIM2->PSC = 7999;
+	TIM2->ARR = delay;
+	TIM2->CNT = 0;
+
+	// Enable interrupt
 	TIM2->DIER |= TIM_DIER_UIE;
 	NVIC_EnableIRQ(TIM2_IRQn);
-	
-	// Set the callback function
+
 	timerCallback = callback;
-	
-	// Start the timer
-	TIM2->CR1 |= TIM_CR1_CEN;
+
+	TIM2->CR1 |= TIM_CR1_CEN;	// Start timer
 }
 
-// Reset the timer with the new interval
-//void timer_reset(uint32_t interval) {
-//	TIM2->CNT = 0;         // Reset the timer counter
-//	TIM2->ARR = interval;  // Reload the ARR register with the new interval
-////	TIM2->CR1 |= TIM_CR1_CEN;
-//
-//	// Re-initialize the timer
-////	timer_init(interval, timerCallback);
-//}
-
-void timer_reset(uint32_t interval) {
-    TIM2->CR1 &= ~TIM_CR1_CEN;       // Stop timer
-    TIM2->CNT = 0;                   // Reset counter
-    TIM2->ARR = interval;            // Update ARR
-    TIM2->EGR |= TIM_EGR_UG;         // Force update event to load ARR
-    TIM2->CR1 |= TIM_CR1_CEN;        // Restart timer
-}
-
-// One-shot timer function
-void timer_one_shot(uint32_t delay, CallbackFunction callback) {
-	// Set the delay and callback function
-	timerCallback = callback;
-//	isOneShot = true;
-	
-	TIM2->CNT = 0;      // Reset the timer counter
-	TIM2->ARR = delay;  // Reload the ARR register with the new interval
-	TIM2->CR1 |= TIM_CR1_CEN;
-	
-	// Re-initialize the timer
-	timer_init(delay, timerCallback);
-}
-
+// Example for the callback function
+// This function will be called if there is a timer overflow
 void blink_leds(void) {
-	// This function will be called if there is a timer overflow
 	static uint8_t led_state = 0;
-	GPIOE->ODR = led_state ? 0xFF00 : 0x0000;  // Toggle LEDs on PE8-PE15
-	led_state = !led_state;  // Flip the state
+	GPIOE->ODR = led_state ? 0xFF00 : 0x0000;	// Toggle LEDs on PE8-PE15
+	led_state = !led_state;  					// Flip the state
 }
 
 
+// For the timer demonstration
 int timerdemo(int interval) {
+//int main (int interval) {
 	enable_clocks();
 	initialise_board();
 
 	// Timer that triggers the callback (blink LED) every x millisecond
-	timer_init((uint32_t)interval, blink_leds); // Input the time interval in millisecond
+	timer_init(1000, blink_leds); // Input the time interval in millisecond
 
 	// Delay for a few seconds or do something
-	for (volatile int i = 0; i < 5000; i++) {
-	}
-
-	int bufferInterval = interval;
+	for (volatile int i = 0; i < 5000000; i++) {}
 
 	timer_reset(100);	// Change the interval
 
-//	while (1) {}
+	// Delay for a few seconds or do something
+	for (volatile int i = 0; i < 5000000; i++) {}
+
+	timer_one_shot(3000, blink_leds);
 }
