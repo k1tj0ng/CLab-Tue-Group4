d0df5967150f8f70913563723fce1078e2d88106
Ribka Anastasya Sitorus <124226902+RibkaAnastasyaSitorus@users.noreply.github.com>
2025-04-14 03:05:50 +1000

    Update timer.c

--- CLab/Src/timer.c
+++ CLab/Src/timer.c
@@ -15,12 +15,12 @@ volatile bool isOneShot = false;
 // Timer that triggers the callback (blink_led) every (interval) millisecond
 void timer_init(uint32_t interval, CallbackFunction callback) {
 	TIM2->CR1 &= ~TIM_CR1_CEN;	// Stop the timer
-	isOneShot = false;			// Flag for periodic timer mode
+	isOneShot = false;		// Flag for periodic timer mode
 
 	// Set up the timer prescaler, for 1khZ frequency = (8MHz:1000)-1
 	TIM2->PSC = 7999;
 	TIM2->ARR = interval;		// Set the time interval
-	TIM2->CNT = 0;				// Reset the counter
+	TIM2->CNT = 0;			// Reset the counter
 
 	TIM2->DIER |= TIM_DIER_UIE;	// Enable interrupt when there is overflow
 	NVIC_EnableIRQ(TIM2_IRQn);	// Enable interrupt in the NVIC
@@ -33,7 +33,7 @@ void timer_init(uint32_t interval, CallbackFunction callback) {
 // Timer module to reset the timer with a new interval
 void timer_reset(uint32_t new_interval) {
 	TIM2->CR1 &= ~TIM_CR1_CEN;	// Stop timer
-	TIM2->CNT = 0;				// Reset counter
+	TIM2->CNT = 0;			// Reset counter
 	TIM2->ARR = new_interval;	// Update ARR
 	TIM2->EGR |= TIM_EGR_UG;	// Force update event to load ARR
 	TIM2->CR1 |= TIM_CR1_CEN;	// Restart timer
@@ -42,7 +42,7 @@ void timer_reset(uint32_t new_interval) {
 // Timer that triggers a one-shot event after (delay) millisecond
 void timer_one_shot(uint32_t delay, CallbackFunction callback) {
 	TIM2->CR1 &= ~TIM_CR1_CEN;	// Stop timer
-	isOneShot = true;			// Flag for oneshot timer mode
+	isOneShot = true;		// Flag for oneshot timer mode
 
 	// Set up the timer prescaler
 	TIM2->PSC = 7999;
@@ -63,7 +63,7 @@ void timer_one_shot(uint32_t delay, CallbackFunction callback) {
 void blink_leds(void) {
 	static uint8_t led_state = 0;
 	GPIOE->ODR = led_state ? 0xFF00 : 0x0000;	// Toggle LEDs on PE8-PE15
-	led_state = !led_state;  					// Flip the state
+	led_state = !led_state;  			// Flip the state
 }
 
 
