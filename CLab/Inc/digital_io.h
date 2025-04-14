a192d2fd6cef2731ff2f3bceff7416bf8a710244
sulfatislam <sisl5208@uni.sydney.edu.au>
2025-04-14 13:11:33 +1000

    fixing set_led_state function

--- CLab/Inc/digital_io.h
+++ CLab/Inc/digital_io.h
@@ -8,16 +8,15 @@
 // Function declarations
 void chase_led(void);
 void button_press(void (*callback)(void));
-void EXTI0_IRQHandler(void);
 
 // Sets the state of the LEDs
-void set_led_state(uint16_t state);
+void set_led_state(uint8_t state);
 
 // Gets the current state of the LEDs
-uint16_t get_led_state(void);  // Should return the value instead of void
+uint8_t get_led_state(void);  // Should return the value instead of void
 
 // External variables
 extern void (*on_button_press)(void);
-extern uint16_t led_state;
+extern uint8_t led_state;
 
 #endif // DIGITAL_IO_H
