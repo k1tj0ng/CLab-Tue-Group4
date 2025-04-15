cfd896b6ec2ecf789b514bd65fd7f09a19db6676
sulfatislam <sisl5208@uni.sydney.edu.au>
2025-04-15 11:17:38 +1000

    Update digital_io.h

--- CLab/Inc/digital_io.h
+++ CLab/Inc/digital_io.h
@@ -8,12 +8,12 @@
 // Function declarations
 void chase_led(void);
 void button_press(void (*callback)(void));
-
+void enable_prescaler();
 // Sets the state of the LEDs
 void set_led_state(uint8_t state);
 
 // Gets the current state of the LEDs
-uint8_t get_led_state(void);  // Should return the value instead of void
+uint8_t get_led_state(void);
 
 // External variables
 extern void (*on_button_press)(void);
